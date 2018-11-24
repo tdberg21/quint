848037c65a46ee5796427de135bb4338215fa653
Tory Dannenberg <rfdberg@gmail.com>
2018-11-24 13:15:23 -0700

    Create GroupInfo component

---
+++ src/Components/GroupInfo/GroupInfo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './GroupInfo.css';
+
+const GroupInfo = () => {
+  return (
+    <div className='groupinfo-component'>
+      <h3 className='groupinfo-text groupinfo-header'>Who We Are</h3>
+      <p className='groupinfo-text groupinfo-paragraph'>Quintet's five partners have that extensive overseas experience and knowledge from conducting and leading clandestine operations in every region of the world, often in hostile environments where the risk of exposure for US Government equities was extraordinary. After their CIA service, Quintet's five partners have all worked as senior business intelligence and security officials or consultants for major US and foreign corporations.</p>
+    </div>
+  )
+};
+
+export default GroupInfo;
