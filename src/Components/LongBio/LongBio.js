73e9c2f6c7b353cfeff6b91d8f2d372b57aaf442
Tory Dannenberg <rfdberg@gmail.com>
2018-11-26 10:51:43 -0700

    Add className to image for CSS purposes

--- src/Components/LongBio/LongBio.js
+++ src/Components/LongBio/LongBio.js
@@ -4,25 +4,12 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const LongBio = (props) => {
-  console.log(props)
-  // const dataToDisplay = bioData.map((bio, index) => {
-  //   return (
-  //   <div className='short-bio' key={index}>
-  //     <img 
-  //       src={bio.source} 
-  //       alt={bio.name} 
-  //       className='short-bio-image'
-  //     />
-  //     <h4>{bio.name}</h4>
-  //     <Link to={`/bios/${bio.lastName}`}>
-  //     <button className='full-bio-button'>Click to View Full Bio</button>
-  //     </Link>
-  //   </div>
-  //   )  
-  // })
   return (
     <div className='longbio-component'>
-      {props.name}
+      <h2>{props.name}</h2>
+      <img src={props.source} alt={props.lastName} className='short-bio-image'/>
+      <p>{props.info}</p>
+      <Link to='/bios'> Back to Group </Link>
     </div>
   )
 };
