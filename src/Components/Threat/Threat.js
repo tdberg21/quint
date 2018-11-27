7af245fb0ea7b5047c5f900b8fc8186252335394
Tory Dannenberg <rfdberg@gmail.com>
2018-11-27 15:21:21 -0700

    Add background to text to make it easier to read,
    add paragraph breaks to long bios, change short bio cards to black background with white text

--- src/Components/Threat/Threat.js
+++ src/Components/Threat/Threat.js
@@ -4,8 +4,10 @@ import './Threat.css';
 const Threat = () => {
   return (
     <div className='threat-component'>
-      <h3 className='threat-text threat-header'>The Threat</h3>
-      <p className='threat-text threat-paragraph'>Risk in overseas markets often evolves gradually, even insidiously, sometimes surfacing as subtle developments or hidden dynamics in a foreign country that go unnoticed until they unleash a crisis.  Risk is even more of a threat in emerging markets that appear commercially promising but remain heavily influenced by deeply rooted cultures and non-western values that can negatively impact the business environment. Managing these risks requires in-depth knowledge and extensive first-hand experience in foreign countries. </p>
+      <div className='blur-bg'>
+        <h3 className='threat-text threat-header'>The Threat</h3>
+        <p className='threat-text threat-paragraph'>Risk in overseas markets often evolves gradually, even insidiously, sometimes surfacing as subtle developments or hidden dynamics in a foreign country that go unnoticed until they unleash a crisis.  Risk is even more of a threat in emerging markets that appear commercially promising but remain heavily influenced by deeply rooted cultures and non-western values that can negatively impact the business environment. Managing these risks requires in-depth knowledge and extensive first-hand experience in foreign countries. </p>
+      </div>
     </div>
   )
 };
