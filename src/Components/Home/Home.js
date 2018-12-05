5eba90466df3a3fb9c74ede364fcb8121e9847a5
Tory Dannenberg <rfdberg@gmail.com>
2018-12-05 08:20:54 -0700

    Update text for all group pages per request

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -6,7 +6,7 @@ const Home = () => {
       <section className='app-body'>
         <div className='blur-bg'>
         <h3 className='threat-text threat-header'>Quint Advisors</h3>
-        <p className='home-paragraph'>​ Quint is a partnership of global risk experts with a unique combination of public and private sector expertise on risks to national security and commercial interests. All the partners served as leaders in CIA’s clandestine service and have also been senior business intelligence and security professionals in major US and foreign companies. Based on this unique experience, Quint’s partners can look behind the headlines and provide their select group of clients with unparalleled insights and risk analysis to navigate the shoals of global risk and chart effective business strategies in the uncertain 21st century global marketplace. </p>
+          <p className='home-paragraph'>​ Quint Advisors is a partnership of global risk experts with a unique combination of public and private sector expertise on risks to national security and commercial interests.  All the partners served as leaders in CIA’s clandestine service and/or as Chief Security Officers for major commercial entities and in those areas of responsibility managed geopolitical, physical, executive, and cyber security threats.  Based on this unique experience, Quint’s partners can look behind the headlines and provide their select and limited group of clients with unparalleled insights and risk analysis to navigate the shoals of global risk and chart effective business and security strategies for the challenges of the 21st century global marketplace. </p>
         </div>
       </section>
     </div>
