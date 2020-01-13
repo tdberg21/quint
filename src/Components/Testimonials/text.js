48aa0f7db4c8189c10565b41c4c6a3be7c81bbcc
Tory Dannenberg <rfdberg@gmail.com>
2020-01-12 17:58:04 -0700

    Update Quint advisors to Q advisors and change some of the text

--- src/Components/Testimonials/text.js
+++ src/Components/Testimonials/text.js
@@ -1,18 +1,18 @@
 export const text = [
   {
-    testimonial: 'The wow factor from the Quint Advisors briefing was off the charts. They were concise, insightful, and helped my firm understand the potential market impact of geo-political events. Drawing on years of experience in looking behind the headlines, I felt I was getting information that gave me an edge.',
+    testimonial: 'The wow factor from the Q Advisors briefing was off the charts. They were concise, insightful, and helped my firm understand the potential market impact of geo-political events. Drawing on years of experience in looking behind the headlines, I felt I was getting information that gave me an edge.',
     source: 'High Net Worth Individual'
   },
   {
-    testimonial: 'I was introduced to the Quint Advisors at the onset of the Russia - Ukraine crisis. The Group’s presentation allowed me to understand the historical antecedents to Russia’s action and helped my firm anticipate the likely response of the West and its impact on markets. I have consulted with the Quint Advisors consistently throughout the crisis and their calls have been spot on. I introduced Quint Advisors to our firm’s Investment Committee and their insights helped us make smart investment decisions over the preceding year. Particularly impactful was the Group’s accessibility. They were always available for a call, in-person briefing or written follow-up.',
+    testimonial: 'I was introduced to the Q Advisors at the onset of the Russia - Ukraine crisis. The Group’s presentation allowed me to understand the historical antecedents to Russia’s action and helped my firm anticipate the likely response of the West and its impact on markets. I have consulted with the Q Advisors consistently throughout the crisis and their calls have been spot on. I introduced Q Advisors to our firm’s Investment Committee and their insights helped us make smart investment decisions over the preceding year. Particularly impactful was the Group’s accessibility. They were always available for a call, in-person briefing or written follow-up.',
     source: 'Major Wall Street Investment Firm'
   },
   {
-    testimonial: 'Quint Advisors was a featured attraction at our annual investor conference. They received the highest rating of any of the presentations in the course of the day. Feedback from attendees focused on the insights gained from experience the Quint’s partners have acquired over the course of extraordinary careers in intelligence. The insights provided and the highly interactive nature of the presentation were entertaining and informative.',
+    testimonial: 'Q Advisors was a featured attraction at our annual investor conference. They received the highest rating of any of the presentations in the course of the day. Feedback from attendees focused on the insights gained from experience the Q’s partners have acquired over the course of extraordinary careers in intelligence. The insights provided and the highly interactive nature of the presentation were entertaining and informative.',
     source: 'Major Financial Services Firm'
   },
   {
-    testimonial: 'My firm has significant exposure in energy markets. Quint Advisors provided me with privileged and unique insights into the impact of the Islamic State’s activities in Iraq and the potential expansion of their influence into North Africa. This level of insight and context can’t be acquired from newspapers or journals. These are the real guys and understand geo-political risk from real world experience.',
+    testimonial: 'My firm has significant exposure in energy markets. Q Advisors provided me with privileged and unique insights into the impact of the Islamic State’s activities in Iraq and the potential expansion of their influence into North Africa. This level of insight and context can’t be acquired from newspapers or journals. These are the real guys and understand geo-political risk from real world experience.',
     source: 'Hedge Fund Portfolio Manager'
   }
 ];
