d9b81ab0925952fe3e94fb2096449af488c34414
Tory Dannenberg <rfdberg@gmail.com>
2020-01-17 10:11:08 -0700

    Update copy on home page

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -3,14 +3,24 @@ import React from 'react';
 const Home = () => {
   return (
     <div>
-      <section className='app-body'>
-        <div className='blur-bg'>
-        <h3 className='threat-text threat-header'>Q Advisors</h3>
-          <p className='home-paragraph'>​ Q Advisors is a partnership of global risk experts with a unique combination of public and private sector expertise on risks to national security and commercial interests.  All the partners served as leaders in CIA’s clandestine service and/or as Chief Security Officers for major commercial entities and in those areas of responsibility managed geopolitical, physical, executive, and cyber security threats.  Based on this unique experience, Q’s partners can look behind the headlines and provide their select and limited group of clients with unparalleled insights and risk analysis to navigate the shoals of global risk and chart effective business and security strategies for the challenges of the 21st century global marketplace. </p>
+      <section className="app-body">
+        <div className="blur-bg">
+          <h3 className="threat-text threat-header">Q Advisors</h3>
+          <p className="home-paragraph">
+            Q Advisors is a partnership of global risk experts with a unique
+            combination of public and private sector expertise on risks to
+            national security and commercial interests. All the partners served
+            as leaders in CIA’s clandestine service and/or as Chief Security
+            Officers for major commercial entities. Q Advisors have proven
+            experience in managing geopolitical, physical, and cyber security
+            threats. Based on this unique experience, Q Advisor’s partners can
+            look behind the headlines and provide their clients with
+            unparalleled insights and geopolitical risk analysis.
+          </p>
         </div>
       </section>
     </div>
-  )
+  );
 };
 
 export default Home;
