375303bf477111d60e2b1e30a1b2843c3f7343a2
Tory Dannenberg <rfdberg@gmail.com>
2020-01-17 10:13:14 -0700

    Update copy in LongBio Products and Threat components

--- src/Components/Products/Products.js
+++ src/Components/Products/Products.js
@@ -7,52 +7,42 @@ const Products = () => {
       <div className="blur-bg">
         <h3 className="groupinfo-text groupinfo-header">What We Offer</h3>
         <p className="groupinfo-text groupinfo-paragraph">
-          ​There may have been more dangerous times in history, but never more
-          complicated. Conflict, terrorism, civil unrest, political instability
-          and cyber all pose threats to our clients’ safety, prosperity and
-          resilience.
+          ​​Conflict, terrorism, civil unrest, political instability and cyber
+          all pose threats to our clients’ safety, prosperity and resilience.
         </p>
         <p className="groupinfo-text groupinfo-paragraph">
-          ​ Q Advisors provides independent, bespoke expert perspective on
-          current and potential geopolitical risk. We are another set of
-          eyes—discreet and experienced—that look at risk factors that affect
-          portfolios and markets. Drawing on deep experience in manage risk and
-          with access to an unparalleled global network, Q Advisors provide
-          timely insight on risks that go beyond the headlines and can provide
-          critical decision advantage.
+          ​Q Advisors provides expert perspective on current and potential
+          geopolitical risk. We are another set of eyes—discreet and
+          experienced—that look at risk factors that affect portfolios and
+          markets. Drawing on deep experience managing risk and with access to
+          unparalleled global networks, Q Advisors provides timely insight on
+          risks that goes behind the headlines and can provide critical decision
+          advantage.
         </p>
+        <p className="groupinfo-text groupinfo-paragraph">Q Advisors:</p>
         <ul className="groupinfo-text groupinfo-paragraph">
           <li>
-            Q Advisors deliver insight on developments across the globe which
-            impact client interests. Our insights help our clients anticipate
-            geopolitical and market risk and, when events occur, we are able to
-            provide real time insight and analysis and turn risk into advantage.
+            Provide insight on developments across the globe which impact client
+            interests and turn risk into advantage.
           </li>
           <li>
-            Q Advisors perform deep diligence on prospective partners or
-            counter-parties. Drawing on deep counterintelligence experience Q
-            Advisors provide our clients a unique diligence and risk management
-            perspective.
+            Produce bespoke written analysis, typically bi-weekly, and on demand
+            during an event.
           </li>
           <li>
-            Q Advisors provide bespoke written analysis, typically bi-weekly,
-            and on demand during an event. Q Advisors are also With five
-            principals, Q Advisors are always in a position to respond to our
-            clients risk management needs
+            Conduct personal meetings or conference calls to address critical
+            client questions and issues.
           </li>
           <li>
-            Review non-financial components of existing portfolios to identify
-            masked risk.
+            Review non-financial components of current or prospective
+            investments to identify masked risk.
           </li>
-          <li>
-            Provide risk mitigation advice on the enduring and ambient threat of
-            cyber.
-          </li>
-          <li>
-            Q Advisors provide trusted, confidential counsel and advice. We
-            understand and respect our clients need for absolute discretion.
-          </li>
+          <li>Advise on the enduring and ambient cyber threats.</li>
         </ul>
+        <p className="groupinfo-text groupinfo-paragraph">
+          Q Advisors delivers trusted, confidential counsel and advice. We
+          understand and respect our clients need for absolute discretion.
+        </p>
         <Link to="/contact" className="group-link">
           Contact Us
         </Link>
