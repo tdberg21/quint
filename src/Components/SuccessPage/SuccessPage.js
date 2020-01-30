165d3520c35dc6a1e9ccb948e11df37a7414de4b
Tory Dannenberg <rfdberg@gmail.com>
2020-01-30 11:18:07 -0700

    Add Success Page component

---
+++ src/Components/SuccessPage/SuccessPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './SuccessPage.css';
+
+const SuccessPage = () => {
+  return (
+    <div className="success-page-component">
+      <p>
+      Success! Thank you for your inquiry.
+      </p>
+    </div>
+  );
+};
+
+export default SuccessPage;
